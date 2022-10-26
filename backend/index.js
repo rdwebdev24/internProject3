dfb41f1a29d3440dcc3e52b3cd8eeba7bd0091dc
rdwebdev24 <rohitdhakad101@gmail.com>
2022-10-26 21:55:14 +0530

    / path

--- backend/index.js
+++ backend/index.js
@@ -8,6 +8,9 @@ const app = express();
 
 app.use(bodyParser.json());
 app.use(cors());
+app.use('/',(req,res)=>{
+     res.status(200).send({status:200,msg:"success"})
+})
 app.use('/task',UserRouter)
 app.use(express.urlencoded({extended:false}))
 
